8728174fad75635d8fbfe7ffc9692b55f4bdbbbb
Eugene Reich <eugene.reich@gmail.com>
2015-10-17 14:19:17 +0300

    Fixed work with jh-list

--- source/blocks/jh-list/jh-list.js
+++ source/blocks/jh-list/jh-list.js
@@ -22,20 +22,27 @@
             ListingBlock,
             JihadBlock,
             {
+                /**
+                 * Returns block selector
+                 */
+
                 getSelector: function()
                 {
                     return '.jh-list';
                 },
 
+                /**
+                 * Return initialize function
+                 */
+
                 initialize: function ($target)
                 {
-                    this.$ = $target;
                     this._checkDistance = JihadCore.throttle(this._checkDistance.bind(this, $target), 500);
                     JihadCore.$win.on('load scroll resize', this._checkDistance);
                     this._checkDistance();
                 },
 
-                getMoreUrl: function()
+                getMoreUrl: function($target)
                 {
                     var $moreBtn,
                         hasMore;
@@ -45,7 +52,7 @@
                         return null;
                     }
 
-                    $moreBtn = this.getMoreBtn();
+                    $moreBtn = this.getMoreBtn($target);
                     hasMore = !!$moreBtn.length;
 
                     if (!hasMore)
@@ -60,45 +67,49 @@
                     ;
                 },
 
-                getMoreBtn: function ()
+                getMoreBtn: function($target)
                 {
-                    return this.$.find(this.getSelector() + '__more');
+                    return $target.find('.jh-list__more');
                 },
 
-                _checkDistance: function ($elem)
+                _checkDistance: function($target)
                 {
-                    var lock = new JihadLock(this.getSelector());
+                    var lock = new JihadLock($target);
 
-                    if (lock.isLocked($elem))
+                    if (lock.isLocked($target))
                         return false;
 
                     var self = this,
                         $win = JihadCore.$win,
                         scrollBottom,
                         bottomEdge,
-                        moreUrl = this.getMoreUrl()
+                        moreUrl = this.getMoreUrl($target)
                     ;
 
-                    if (moreUrl && !this.request)
+                    if (moreUrl)
                     {
                         scrollBottom = $win.scrollTop() + $win.height();
-                        bottomEdge = $elem.offset().top + $elem.height();
+                        bottomEdge = $target.offset().top + $target.height();
 
                         if (bottomEdge - scrollBottom < 1000)
                         {
-                            this.getMoreBtn().unbind().remove();
+                            lock.lock($target);
 
-                            this.request = $.ajax(
+                            $target.addClass('jh-list__loading');
+                            $target
+                                .find('.jh-list__toggle')
+                                .toggleClass('jh-list__hidden')
+                            ;
+
+                            this.getMoreBtn($target).unbind();
+
+                            $.ajax(
                                     {
                                         url: moreUrl,
                                         dataType: 'json',
-                                        complete: function () {
-                                            self.request = null;
-                                            lock.unlock($elem);
-                                        },
-                                        beforeSend: function()
+                                        complete: function ()
                                         {
-                                            lock.lock($elem);
+                                            lock.unlock($target);
                                         }
                                     }
                                 )
@@ -109,13 +120,22 @@
                                             $html
                                         ;
 
+                                        self.getMoreBtn($target).remove();
+
+                                        lock.unlock($target);
+
+                                        $target.removeClass('jh-list__loading');
+                                        $target
+                                            .find('.jh-list__toggle')
+                                            .toggleClass('jh-list__hidden')
+                                        ;
+
                                         if (result.html)
                                         {
                                             $html = $(result.html);
-                                            $elem.append($html);
+                                            $target.append($html);
+
                                             JihadCore.blocksRun($html);
-                                            self.request = null;
-                                            lock.unlock($elem);
                                         }
                                     }
                                 )
