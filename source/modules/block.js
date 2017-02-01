6f5458a1e98dc5772bb3066777f3dfea453df2f9
StepanMas <stepan.mas.work@gmail.com>
2017-02-01 19:29:12 +0400

    fix bug 1 in CORE-103

--- source/modules/block.js
+++ source/modules/block.js
@@ -2,58 +2,62 @@
     function ()
     {
         var JihadBlock = window.JihadBlock = {};
-        
+
         /**
          * Returns main block selector and children elements
          */
-        
+
         JihadBlock.getSelector = function ()
         {
             return null;
         };
-        
+
         JihadBlock.sel = function (q, n)
         {
             if (typeof q === 'number') return this.getSelector().slice(q);
 
             return q ? (this.getSelector() + '-' + q).slice(n) : this.getSelector();
         };
-        
+
         /**
          * Communication between blocks
          */
-        
+
         JihadBlock.emit = function (event)
         {
             if (!event) throw new Error('Event name is not set.');
-            
+
             var props = $.makeArray(arguments).slice(1);
-            
+
             $.each(
                 JihadCore.blocks,
                 function (selector)
                 {
-                    var arg = props.slice(0);
-                    
-                    arg.unshift(selector);
-                    $(selector).triggerHandler(event, arg);
+                    $(selector).triggerHandler(
+                        $.Event(
+                            event, {
+                                __jihadSelector: selector
+                            }
+                        ),
+                        props
+                    );
                 }
             );
         };
-        
+
         /**
          * Returns bindings
          */
-        
+
         JihadBlock.getBindings = function ()
         {
             return [];
         };
-        
+
         /**
          * Returns initialized flag for block
          */
-        
+
         JihadBlock.isInitialized = function ($target)
         {
             var initialized = $target.data('jihad-initialized');
@@ -63,70 +67,90 @@
                 if (selector in initialized)
                     return true;
             }
-            
+
             return false;
         };
-        
+
         /**
          * Function to apply all bindings
          */
-        
+
         JihadBlock.applyBindings = function ($target)
         {
+            var block = this;
+
             $.each(
                 this.getBindings($target),
                 function (index, binding)
                 {
                     $target.on.apply(
                         $target,
-                        binding
+                        [
+                            binding[0],
+                            typeof binding[1] === 'string' ? binding[1] : handler(binding[1]),
+                            binding[2] ? handler(binding[2]) : undefined
+                        ]
                     );
+
+                    function handler(prime)
+                    {
+                        return function (e)
+                        {
+                            if (e.__jihadSelector)
+                            {
+                                if (block.getSelector() === e.__jihadSelector)
+                                    prime.apply(block, arguments);
+                            }
+                            else
+                                prime.apply(block, arguments);
+                        }
+                    }
                 }
             );
         };
-        
+
         /**
          * Returns initialize function
          */
-        
+
         JihadBlock.initialize = function ($target)
         {
         };
-        
+
         /**
          * Returns finalize function
          */
-        
+
         JihadBlock.finalize = function ($target)
         {
         };
-        
+
         /**
          * Run block
          */
-        
+
         JihadBlock.run = function ($target)
         {
             var self = this;
-            
-            function _tmp(){}
-            
+
+            function _tmp() {}
+
             $.each(
                 $target,
                 function ()
                 {
                     var $elem = $(this);
-                    
+
                     _tmp.prototype = self;
                     self           = new _tmp();
                     _tmp.prototype = null;
-                    
+
                     if (!self.isInitialized($elem))
                     {
                         var initialized                 = $elem.data('jihad-initialized') || {};
                         initialized[self.getSelector()] = 1;
                         $elem.data('jihad-initialized', initialized);
-                        
+
                         self.el = function (q)
                         {
                             return q ? $(self.getSelector() + '-' + q, $elem) : $elem;
