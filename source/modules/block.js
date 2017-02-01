aafe5722ee71a86aa20fe46eda60b88b70dfbf35
StepanMas <stepan.mas.work@gmail.com>
2017-02-01 19:31:11 +0400

    back indents

--- source/modules/block.js
+++ source/modules/block.js
@@ -2,33 +2,33 @@
     function ()
     {
         var JihadBlock = window.JihadBlock = {};
-
+        
         /**
          * Returns main block selector and children elements
          */
-
+        
         JihadBlock.getSelector = function ()
         {
             return null;
         };
-
+        
         JihadBlock.sel = function (q, n)
         {
             if (typeof q === 'number') return this.getSelector().slice(q);
-
+            
             return q ? (this.getSelector() + '-' + q).slice(n) : this.getSelector();
         };
-
+        
         /**
          * Communication between blocks
          */
-
+        
         JihadBlock.emit = function (event)
         {
             if (!event) throw new Error('Event name is not set.');
-
+            
             var props = $.makeArray(arguments).slice(1);
-
+            
             $.each(
                 JihadCore.blocks,
                 function (selector)
@@ -44,20 +44,20 @@
                 }
             );
         };
-
+        
         /**
          * Returns bindings
          */
-
+        
         JihadBlock.getBindings = function ()
         {
             return [];
         };
-
+        
         /**
          * Returns initialized flag for block
          */
-
+        
         JihadBlock.isInitialized = function ($target)
         {
             var initialized = $target.data('jihad-initialized');
@@ -67,18 +67,18 @@
                 if (selector in initialized)
                     return true;
             }
-
+            
             return false;
         };
-
+        
         /**
          * Function to apply all bindings
          */
-
+        
         JihadBlock.applyBindings = function ($target)
         {
             var block = this;
-
+            
             $.each(
                 this.getBindings($target),
                 function (index, binding)
@@ -91,7 +91,7 @@
                             binding[2] ? handler(binding[2]) : undefined
                         ]
                     );
-
+                    
                     function handler(prime)
                     {
                         return function (e)
@@ -108,49 +108,49 @@
                 }
             );
         };
-
+        
         /**
          * Returns initialize function
          */
-
+        
         JihadBlock.initialize = function ($target)
         {
         };
-
+        
         /**
          * Returns finalize function
          */
-
+        
         JihadBlock.finalize = function ($target)
         {
         };
-
+        
         /**
          * Run block
          */
-
+        
         JihadBlock.run = function ($target)
         {
             var self = this;
-
+            
             function _tmp() {}
-
+            
             $.each(
                 $target,
                 function ()
                 {
                     var $elem = $(this);
-
+                    
                     _tmp.prototype = self;
                     self           = new _tmp();
                     _tmp.prototype = null;
-
+                    
                     if (!self.isInitialized($elem))
                     {
                         var initialized                 = $elem.data('jihad-initialized') || {};
                         initialized[self.getSelector()] = 1;
                         $elem.data('jihad-initialized', initialized);
-
+                        
                         self.el = function (q)
                         {
                             return q ? $(self.getSelector() + '-' + q, $elem) : $elem;
