c3d992e9ab49e3ba706ffb40f1e1ffa3bf37cc0c
StepanMas <stepan.mas.work@gmail.com>
2017-04-25 18:12:33 +0400

    update tests and add JihadModEl BM-14

--- test/all.js
+++ test/all.js
@@ -1,6 +1,6 @@
-!function()
+!function ()
 {
-    var Module   = {};
+    var Module = {};
     
     JihadCore.blockRegister(
         $.extend(
@@ -31,7 +31,7 @@
                             {
                                 /* Emitter
                                  ================================================== */
-            
+                                
                                 describe(
                                     "Difference events", function ()
                                     {
@@ -48,13 +48,13 @@
                     ];
                 },
                 
-                initialize: function(block)
+                initialize: function (block)
                 {
                     var self = this;
                     
                     /* Get selectors
-                    ================================================== */
-    
+                     ================================================== */
+                    
                     describe(
                         "Get selectors", function ()
                         {
@@ -64,28 +64,28 @@
                                     expect(self.getSelector()).toBe('.block1');
                                 }
                             );
-    
+                            
                             it(
                                 ".sel", function ()
                                 {
                                     expect(self.sel()).toBe('.block1');
                                 }
                             );
-    
+                            
                             it(
                                 ".el", function ()
                                 {
                                     expect(self.el()).toBe(block);
                                 }
                             );
-    
+                            
                             it(
                                 "Child", function ()
                                 {
                                     expect(self.sel('child')).toBe('.block1-child');
                                 }
                             );
-    
+                            
                             it(
                                 "Child element", function ()
                                 {
@@ -97,10 +97,10 @@
                     
                     
                     /* Emitter
-                    ================================================== */
+                     ================================================== */
                     
                     setTimeout(
-                        function()
+                        function ()
                         {
                             self.emit('some-event', 'some-data');
                         },
@@ -112,6 +112,8 @@
     );
 }();
 
+/* Block2
+ ================================================== */
 
 !function ()
 {
@@ -142,7 +144,7 @@
                     return [
                         [
                             'some-event',
-                            function(e, data)
+                            function (e, data)
                             {
                                 /* Emitter
                                  ================================================== */
@@ -156,14 +158,14 @@
                                                 expect(e.type).toBe('some-event');
                                             }
                                         );
-    
+                                        
                                         it(
                                             "Some-data", function ()
                                             {
                                                 expect(data).toBe('some-data');
                                             }
                                         );
-    
+                                        
                                         it(
                                             "Difference events", function ()
                                             {
@@ -179,6 +181,257 @@
                 
                 initialize: function ()
                 {
+                }
+            }
+        )
+    );
+}();
+
+/* Block 3
+ ================================================== */
+
+!function ()
+{
+    var Module = {};
+    
+    JihadCore.blockRegister(
+        $.extend(
+            Module,
+            JihadBlock,
+            {
+                /**
+                 * Returns block selector
+                 */
+                
+                getSelector: function ()
+                {
+                    return '.block3';
+                },
+                
+                /**
+                 * Returns block bindins
+                 */
+                
+                getBindings: function ()
+                {
+                    var self = this;
+                    
+                    return [
+                        [
+                            'input',
+                            '> input',
+                            function ()
+                            {
+                                console.log(self.model().get(), self.sel());
+                            }
+                        ]
+                    ];
+                },
+                
+                initialize: function ()
+                {
+                    var self = this;
+                    
+                    JihadModel.init(this);
+                    
+                    /* Model
+                     ================================================== */
+                    
+                    this.model().set('varName1', 'value');
+                    
+                    describe(
+                        "Get model", function ()
+                        {
+                            var foo;
+                            
+                            beforeEach(
+                                function ()
+                                {
+                                    foo = self.model().get()
+                                }
+                            );
+                            
+                            it(
+                                "Properly objects", function ()
+                                {
+                                    expect(foo).toEqual(
+                                        jasmine.objectContaining(
+                                            {
+                                                varName1: "value"
+                                            }
+                                        )
+                                    );
+                                }
+                            );
+                            
+                            it(
+                                'Get item', function ()
+                                {
+                                    expect(self.model().get('varName1')).toBe('value');
+                                }
+                            )
+                        }
+                    );
+                    
+                    describe(
+                        "Model apply", function ()
+                        {
+                            self.model().apply();
+                            
+                            it(
+                                'In input of node', function ()
+                                {
+                                    expect(self.el().find('input' + self.sel('model')).val()).toBe('value');
+                                }
+                            );
+                            
+                            it(
+                                'In block of node like text', function ()
+                                {
+                                    expect(self.el().find('h3' + self.sel('model')).text()).toBe('value');
+                                }
+                            )
+                        }
+                    );
+                }
+            }
+        )
+    );
+}();
+
+/* Block4
+ ================================================== */
+
+!function ()
+{
+    var Module = {};
+    
+    JihadCore.blockRegister(
+        $.extend(
+            Module,
+            JihadBlock,
+            {
+                /**
+                 * Returns block selector
+                 */
+                
+                getSelector: function ()
+                {
+                    return '.block4';
+                },
+                
+                /**
+                 * Returns block bindins
+                 */
+                
+                getBindings: function ()
+                {
+                    var self = this;
+                    
+                    return [
+                        [
+                            'input',
+                            '> input',
+                            function ()
+                            {
+                                //console.log(self.model().get(), self.sel());
+                            }
+                        ],
+                        
+                        [
+                            'model:change',
+                            function (e, key, old_value, value)
+                            {
+                                console.warn(key === 'varName2');
+                                console.warn(old_value === 'Model was replace');
+                                console.warn(value === 'Button click updated the value');
+                            }
+                        ],
+                        
+                        [
+                            'model:applied',
+                            function (e)
+                            {
+                                console.log(e.type === 'model:applied', '(2 false it\'s normal)');
+                            }
+                        ],
+                        
+                        [
+                            'click',
+                            this.sel('change-data'),
+                            function ()
+                            {
+                                self.model().set('varName2', 'Button click updated the value');
+                            }
+                        ],
+                        
+                        [
+                            'click',
+                            this.sel('change-data-unobtrusive'),
+                            function ()
+                            {
+                                self.model(
+                                    {
+                                        varName2: 'Data set unobtrusive'
+                                    }
+                                ).apply();
+                            }
+                        ]
+                    ];
+                },
+                
+                initialize: function ()
+                {
+                    var self = this;
+                    
+                    JihadModel.init(this);
+                    
+                    describe(
+                        "Model replace", function ()
+                        {
+                            window.newModel = {varName2: 'Value was set'};
+                            self.model(window.newModel).apply();
+                            
+                            var foo;
+                            
+                            beforeEach(
+                                function ()
+                                {
+                                    foo = self.model().get()
+                                }
+                            );
+                            
+                            self.model({varName2: 'Model was replace'}).apply();
+                            
+                            it(
+                                "Properly objects", function ()
+                                {
+                                    expect(window.newModel).toEqual(
+                                        jasmine.objectContaining({varName2: 'Value was set'})
+                                    );
+                                }
+                            );
+                        }
+                    );
+                    
+                    describe(
+                        "Model format data", function ()
+                        {
+                            it(
+                                "To URL params", function ()
+                                {
+                                    expect(self.model().toParams()).toBe('varName2=Model%20was%20replace');
+                                }
+                            );
+                            
+                            it(
+                                "To JSON", function ()
+                                {
+                                    expect(self.model().serialize()).toBe('{"varName2":"Model was replace"}');
+                                }
+                            );
+                        }
+                    );
                 }
             }
         )
